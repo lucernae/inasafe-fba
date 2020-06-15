aba7beae1cd1956fb7ab0f986a1ef593a8c77dd4
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2020-06-15 16:55:52 +0700

    Add more configuration to django backend
    - Allow Python Requests module to fetch unsecure
      https (used for internal mapserver proxying)
    - Allow path name resolve of the fixture data
      for layer styles

--- mapserver/django/django_project/core/settings/project.py
+++ mapserver/django/django_project/core/settings/project.py
@@ -12,6 +12,9 @@ from .contrib import *  # noqa
 
 # Due to profile page does not available,
 # this will redirect to home page after login
+from .utils import ABS_PATH
+import ast
+
 LOGIN_REDIRECT_URL = '/'
 
 # How many versions to list in each project box
@@ -37,4 +40,6 @@ INSTALLED_APPS += (
 MAPSERVER_PUBLIC_WMS_URL = os.environ.get('MAPSERVER_PUBLIC_WMS_URL', None)
 MAPSERVER_PUBLIC_OWS_URL = os.environ.get('MAPSERVER_PUBLIC_OWS_URL', None)
 MAPSERVER_PUBLIC_SLD_URL = os.environ.get('MAPSERVER_PUBLIC_SLD_URL', None)
-FIXTURES = '/home/web/fixtures'
+FIXTURES = ABS_PATH('../../fixtures')
+# Specify settings to ignore Python Requests SSL verification
+REQUESTS_SSL_VERIFY = ast.literal_eval(os.environ.get('REQUESTS_SSL_VERIFY', 'False'))
